9f25133426b4a87d2c0d591241dba901868d9261
ZorroSec <joseiraildesciprianoribeiro@gmail.com>
2023-03-21 18:52:17 -0300

    criando a priimeira endpoint do app

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const { app, express } = require('./js/routes.js')
+app.use('/view/', express.static('./view'))
+app.use('/scripts/css/', express.static('./scripts/css/'))
+app.use('/scripts/javascript/', express.static('./scripts/javascript/'))
+
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname + '/index.html')
+})
