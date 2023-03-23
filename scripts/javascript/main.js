620ef78fc8fb72633eefca6f84f201b7ed053898
ZorroSec <joseiraildesciprianoribeiro@gmail.com>
2023-03-22 21:19:05 -0300

    criando uma mini area de if em main

--- scripts/javascript/main.js
+++ scripts/javascript/main.js
@@ -2,3 +2,11 @@ const btn = document.getElementById('btn')
 const username = document.getElementById('username')
 const post = document.getElementById('post')
 const postConteiner = document.getElementById('postConteiner')
+if(btn){
+    btn.onclick = ()=>{
+        if(username != 'ZezoCentral'){
+            postConteiner.innerText=`
+            `
+        }
+    }
+}
