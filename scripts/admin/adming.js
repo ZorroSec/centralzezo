303c8e9e0e5be352e18ebba9846a0c0754346ac1
ZorroSec <joseiraildesciprianoribeiro@gmail.com>
2023-03-24 15:23:16 -0300

    errors

--- scripts/admin/adming.js
+++ scripts/admin/adming.js
@@ -3,32 +3,31 @@ const password = document.getElementById('password')
 const btnLogin = document.getElementById('btnLogin')
 const response = document.getElementById('response')
 const infopost = document.getElementById('infoPost')
-if(btnLogin){
-    if(username == 'Zezao'){
-        btnLogin.onclick = ()=>{
-            infopost.innerHTML=`
-                <div class="inputUsername">
-                    <input type="text" name="username" id="username" placeholder="Username...">
-                </div>
-                <div class="inputHour">
-                    <input type="date" name="hour" id="datetimelocal">
-                </div>
-                <div class="inputPostArea">
-                    <textarea name="post" id="post" cols="30" rows="10">Hello World</textarea>
-                </div>
-                <div class="btn">
-                    <button id="btn">
-                        Send...
-                    </button>
-                </div>
-                <div id='postConteiner'>
-                
-                </div>
-            `
-        }
-    } else {
-        response.innerHTML=`
-            <h2 style='color: red;'>INVALID USERNAME!!!</h2>
+
+btnLogin.onclick = ()=>{
+    if(username.value == 'Zezao'){
+        infopost.innerHTML=`
+            <div class="inputUsername">
+                <input type="text" name="username" id="username" placeholder="Username...">
+            </div>
+            <div class="inputHour">
+                <input type="date" name="hour" id="datetimelocal">
+            </div>
+            <div class="inputPostArea">
+                <textarea name="post" id="post" cols="30" rows="10">Hello World</textarea>
+            </div>
+            <div class="btn">
+                <button id="btn">
+                    Send...
+                </button>
+            </div>
+            <div id='postConteiner'>
+            </div>
         `
+    } else {
+        postConteiner.innerHTML=`
+            <h2 style='color:red;'>USERNAME IS INVALID!!!</h2>
+        `
+        console.error('Username is invalid!!')
     }
 }
