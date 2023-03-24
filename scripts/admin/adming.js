f54c7d6944354f92b65139e5d1cba82b72083540
ZorroSec <joseiraildesciprianoribeiro@gmail.com>
2023-03-24 15:01:26 -0300

    ¬

---
+++ scripts/admin/adming.js
@@ -0,0 +1,24 @@
+const username = document.getElementById('username')
+const password = document.getElementById('password')
+const btnLogin = document.getElementById('btnLogin')
+const response = document.getElementById('response')
+if(btnLogin){
+    btnLogin.onclick = ()=>{
+        response.innerHTML=`
+            <div class="inputUsername">
+                <input type="text" name="username" id="username" placeholder="Username...">
+            </div>
+            <div class="inputHour">
+                <input type="date" name="hour" id="datetimelocal">
+            </div>
+            <div class="inputPostArea">
+                <textarea name="post" id="post" cols="30" rows="10">Hello World</textarea>
+            </div>
+            <div class="btn">
+                <button id="btn">
+                    Send...
+                </button>
+            </div>
+        `
+    }
+}
